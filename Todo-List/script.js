743b178e16f83cb58ff1cc813d175f0c120bfe90
jamie7dev <jamie7dev@gmail.com>
2023-03-31 16:11:51 +0900

    [feat] todo list 추가, input value 비우기

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -1,6 +1,13 @@
 const keyCodeCheck = function () {
   if (window.event.keyCode === 13) {
-    const inputValue = document.querySelector("#todo-input").value;
-    console.log(inputValue);
+    const todoList = document.querySelector("#todo-list");
+    const todoInput = document.querySelector("#todo-input");
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    todoInput.value = "";
   }
 };
