8ce3ec9c18f26688c7e783402bac2bd5a5edf0aa
jamie7dev <jamie7dev@gmail.com>
2023-04-01 11:33:50 +0900

    [feat] todo list 개별 삭제, 전체 삭제 구현

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -10,16 +10,26 @@ const createTodo = function () {
     newLi.classList.toggle("complete");
   });
 
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
   newSpan.textContent = todoInput.value;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
-  console.log(newLi);
   todoInput.value = "";
 };
 
 const keyCodeCheck = function () {
   if (window.event.keyCode === 13 && todoInput.value !== "") {
     createTodo();
+  }
+};
+
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
   }
 };
